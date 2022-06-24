d585ed6db4dcadaf3e748734a453f157bae7b35c
amecreate <amelieasmile@gmail.com>
2022-06-24 08:03:02 -0400

    working file for const {createApp}=Vue

--- app.js
+++ app.js
@@ -6,14 +6,14 @@
 
 const { createApp } = Vue 
 
-// createApp({
-//     template: '<h1>Hello World</h1>'
-// }).mount('#app')
-
 createApp({
-    data() {
-        return {
-            message: 'Hello Vue!'
-        }
-    }
+    template: '<h1>Hello World</h1>'
 }).mount('#app')
+
+// createApp({
+//     data() {
+//         return {
+//             message: 'Hello Vue!'
+//         }
+//     }
+// }).mount('#app')
