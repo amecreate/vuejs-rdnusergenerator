6782b3cb3b2f703c6b4414e22ed58cbac4d79a92
amecreate <amelieasmile@gmail.com>
2022-06-24 16:28:15 -0400

    v-bind in attribute

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const app = Vue.createApp({
     data(){
         return {
-            firstName: "John"
+            firstName: "John",
+            lastName: "Doe",
+            email: "john@gmail.com",
+            gender: "male",
+            picture: "http://randomuser.me/api/portraits/men/10.jpg"
         }
     }
 })
